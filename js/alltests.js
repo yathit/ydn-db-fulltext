4e1cea21425e5f0eb733628aba930605e3c9f11c
kyawtun <okisan.2008@gmail.com>
2013-08-04 12:52:47 +0800

    compiled OK.

--- js/alltests.js
+++ js/alltests.js
@@ -2,4 +2,4 @@ var _idb_allTests = [];
 var _websql_allTests = [];
 var _ls_allTests = [];
 var _ud_allTests = [];
-var _allTests = ["normalizer/english/en_normalizer_test.html", "normalizer/unicode/unicode_test.html", "analyzer/analyzer_test.html", "query/scoring_engine_store_test.html", "query/query_engine_test.html", "query/search_query_test.html", "query/resultset_test.html"];
+var _allTests = ["query/search_query_test.html", "query/query_engine_test.html", "query/scoring_engine_store_test.html", "query/resultset_test.html"];
